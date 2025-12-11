d9605e91d875a1619c9b32826f4e2babe89a4a2f
Thomas Derome <thomas.derome@epitech.eu>
2025-12-11 13:07:43 +0100

    [+] => project file for simple editing project

---
+++ data/projet.js
@@ -0,0 +1,39 @@
+const project = [
+    {
+        "name": "Radio-Stream",
+        "description": "Scrappe les flux de radios françaises et les rend accessibles via une page web interactive avec un lecteur, une recherche et des sauvegardes.",
+        "tech": "Flask, bs4, Requests, HTML/CSS/JS"
+    },
+    {
+        "name": "Vote-Bot",
+        "description": "Automatise les votes sur des serveurs de jeu pour obtenir des récompenses, le tout piloté par un panel de contrôle web.",
+        "tech": "Selenium, Flask, HTML/CSS/JS"
+    },
+    {
+        "name": "Bot-JDR",
+        "description": "Bot Discord conçu pour le jeu de rôle, permettant de sauvegarder et d'interagir avec des fiches de personnages et des inventaires, lancer de dé.",
+        "tech": "discord.py, JSON"
+    },
+    {
+        "name": "BotyBot",
+        "description": "Un bot de modération complet pour Discord, incluant un système de tickets et les commandes essentielles (ban, kick, mute).",
+        "tech": "discord.py, SQLite"
+    },
+    {
+        "name": "Bbox-api",
+        "description": "Une API Python pour interagir avec la Bbox et configurer les règles du pare-feu (NAT/PAT).",
+        "tech": "Python, Requests"
+    },
+    {
+        "name": "Crackers",
+        "description": "Une aplication de scrapping qui recherche et recherche les cracks de jeux vidéo disponibles sur différentes sources web.",
+        "tech": "Python, bs4, Requests"
+    },
+    {
+        "name": "NetflixKode-expired",
+        "description": "Script pour modifier ou retrouver le mot de passe d'une clé de connexion pour une extension Netlfix Kodi en utilisant des techniques de cryptographie.",
+        "tech": "Python, PyCryptodome"
+    }
+];
+
+export { project }
